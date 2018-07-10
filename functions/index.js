f66543cac7f2097fcbbe579ee342309f2457cc01
Rob Lindsey <rob@roblindsey.com>
2018-07-10 14:17:11 -0400

    added PUT and GET tag routes. express-validator input validation.

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,7 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 const slugify = require('slugify');
 const cors = require('cors')({ origin: true });
+const { check, validationResult } = require('express-validator/check');
 
 const app = express();
 
@@ -34,7 +35,16 @@ app.use(cors);
 
 //Routes... all routes protected
 // GET one item
-app.get('/read/:slug', authorizeMe, (req, res) => {
+app.get('/read/:slug', authorizeMe, [
+  // Validate slug
+  check('slug').matches(/^[a-zA-Z0-9-_]+$/)
+], (req, res) => {
+  // Return input validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  // Firestore collection
   const collectionRef = admin.firestore().collection('clips');
   const query = collectionRef.where('slug', '==', req.params.slug);
 
@@ -69,16 +79,65 @@ app.get('/read', authorizeMe, (req, res) => {
       return res.send('Error getting documents');
     });
 });
-// POST new item
-app.post('/save', authorizeMe, (req, res) => {
-  const clip = req.body;
+// GET tag list 
+app.get('/tag/:tag', authorizeMe, [
+  check('tag').isAlphanumeric()
+], (req, res) => {
+  // Return input validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  const tag = req.params.tag;
+  const collectionRef = admin.firestore().collection('clips');
+  const query = collectionRef.where(`tags.${tag}`, '==', true);
 
-  // TODO validate input values
+  return query.get()
+    .then(querySnapshot => {
+      let results = [];
+      if (querySnapshot.size > 0) {
+        querySnapshot.forEach(doc => {
+          results.push(doc.data());
+        });
+        return res.json(results);
+      }
+      return res.send('No such document');
+    })
+    .catch(err => {
+      console.error('Document not found: ', err.message);
+      return res.send('Document not found');
+    });
+});
+// POST new item
+app.post('/save', authorizeMe, [
+  // Validate inputs
+  check('url').not().isEmpty().isURL({ require_protocol: true }),
+  check('tags.*').isAlphanumeric()
+], (req, res) => {
+  // Return input validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  // Reducer function for array to obj.
+  const tagReducer = function(tagObj, tag) {
+    if (!tagObj[tag]) {  // Skip duplicate tags
+      tagObj[tag] = true;
+    }
+    return tagObj;
+  }
+  // Grab POST content
+  const clip = req.body;
+  // Convert tags array to object. For querying in Firestore.
+  let tags = {};
+  if (req.body.tags.length > 0) {
+    tags = req.body.tags.reduce(tagReducer, {});
+  }
 
   return admin.firestore().collection('clips')
     .add({
       url: clip.url,
-      tags: clip.tags,
+      tags: tags,
       date: Date.now()
     })
     .then((writeResult) => {
@@ -87,11 +146,75 @@ app.post('/save', authorizeMe, (req, res) => {
     .catch(err => console.error(err.message));
 });
 // PUT update item
-app.put('/save/:slug', authorizeMe, (req, res) => {
-  res.send('PUT');
+app.put('/save/:slug', authorizeMe, [
+  // Validate slug
+  check('slug').matches(/^[a-zA-Z0-9-_]+$/),
+  check('title').not().isEmpty().escape(),
+  check('summary').not().isEmpty().isLength({ max: 180 }).escape(),
+  check('tags.*').isAlphanumeric()
+], (req, res) => {
+  // Return input validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  // Firestore collection
+  const collectionRef = admin.firestore().collection('clips');
+  const query = collectionRef.where('slug', '==', req.params.slug);
+
+  // Reducer function for array to obj.
+  const tagReducer = function(tagObj, tag) {
+    if (!tagObj[tag]) {  // Skip duplicate tags
+      tagObj[tag] = true;
+    }
+    return tagObj;
+  }
+  // Grab POST content
+  const clip = req.body;
+  // Convert tags array to object. For querying in Firestore.
+  let tags = {};
+  if (req.body.tags.length > 0) {
+    tags = req.body.tags.reduce(tagReducer, {});
+  }
+
+  return query.get()
+    .then(querySnapshot => {
+      let id = '';
+      if (querySnapshot.size > 0) {
+        querySnapshot.forEach(doc => {
+          id = doc.id;
+        });
+        return collectionRef.doc(id);
+      }
+      return res.send('No such document');
+    })
+    .then(doc => {
+      console.log('Updating... ', doc.id);
+      return doc.update({
+        title: clip.title,
+        summary: clip.summary,
+        tags: tags
+      });
+    })
+    .then(result => {
+      return res.status(200).send('Updated!');
+    })
+    .catch(err => {
+      console.error('Error updating: ', err.message);
+      return res.status(500).send('Error updating');
+    });
 });
 // DELETE item
-app.delete('/delete/:slug', authorizeMe, (req, res) => {
+app.delete('/delete/:slug', authorizeMe, [
+  // Validate slug
+  check('slug').matches(/^[a-zA-Z0-9-_]+$/)
+], (req, res) => {
+  // Return input validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  // Firestore collection
   const collectionRef = admin.firestore().collection('clips');
   const query = collectionRef.where('slug', '==', req.params.slug);
 
@@ -182,3 +305,5 @@ exports.saveReadable = functions.firestore.document('/clips/{documentId}').onCre
     .then(res => console.log('Added reader-friendly content: ', res))
     .catch(err => console.error(err.message));
 });
+
+// TODO dedupe tags after update
